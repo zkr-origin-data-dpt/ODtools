b976e75ddd8b1387e08f55a5dede1eb89cd7a816
bojingqian <jqian_bo@163.com>
2019-06-26 16:04:33 +0800

    add kafka client

---
+++ KfkClient_pykafka/conf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Email: jqian_bo@163.com
+# @Author: JingQian Bo
+# @Create Time: 2018/9/27-下午3:48
+
+demo_Sever = '10.211.55.5:9092'
+demo_Key = b'spider_demo'
+demo_Topic = b'test'
+demo_GroupID = 'test2'

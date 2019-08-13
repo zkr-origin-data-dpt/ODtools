404c5c2f08ca54e62869b0e8066596d16fd852aa
李长喜 <m15506590756@163.com>
2019-08-13 17:02:54 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ODtools",
-    version="1.1.6",
+    version="1.1.7",
     author="zkrPython",
     author_email="178031608@qq.com",
     description="zkrTools",

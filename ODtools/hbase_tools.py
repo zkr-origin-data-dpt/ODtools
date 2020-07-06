531372d9a2bed1794fbd1f0648b85f262603f19d
178031608@qq.com <ma343659>
2020-07-06 11:02:10 +0800

    hbase 扫表更新

--- ODtools/hbase_tools.py
+++ ODtools/hbase_tools.py
@@ -202,7 +202,6 @@ class HBaseClient(metaclass=Singleton):
                 dict_data['rowkey'] = start_row
                 yield dict_data
             try:
-                self.reconnect()
                 tscan = TScan(startRow=str(start_row).encode() if start_row else None)
                 scan_id = self.client.openScanner(str(hbase_table).encode(), tscan)
                 row_list = self.client.getScannerRows(scan_id, 1000)

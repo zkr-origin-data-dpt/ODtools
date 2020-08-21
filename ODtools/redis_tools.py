f3806b0c7dfcd97e8956fefb86826d0008011350
178031608@qq.com <ma343659>
2020-08-21 14:30:40 +0800

    ODtools增加redis连接对象密码版本

--- ODtools/redis_tools.py
+++ ODtools/redis_tools.py
@@ -1,3 +1,5 @@
+import random
+
 import redis
 import rediscluster
 from ODtools.singleton_tools import Singleton
@@ -7,17 +9,24 @@ class RedisClient(metaclass=Singleton):
     """
     redis tools client
     """
-    def __init__(self, host: str, port: int, cluster: bool = False):
+
+    def __init__(self, host: str, port: int, cluster: bool = False, password: str = False):
         """
         :param host: redis host
         :param port: redis port
         :param cluster: is redis cluster or not
         """
-        self.host = host
-        self.port = port
+        self.host = random.choices(host) if type(host) == list else host
+        self.port = random.choices(port) if type(port) == list else port
         redis_type = rediscluster.StrictRedisCluster if cluster else redis.StrictRedis
-
-        self.redis = redis_type(host=self.host,
+        if password:
+            self.redis = redis_type(host=self.host,
+                                    port=self.port,
+                                    password=password,
+                                    decode_responses=True,
+                                    socket_connect_timeout=5)
+        else:
+            self.redis = redis_type(host=self.host,
                                 port=self.port,
                                 decode_responses=True,
                                 socket_connect_timeout=5)
@@ -26,4 +35,6 @@ class RedisClient(metaclass=Singleton):
 
 
 if __name__ == '__main__':
-    pass
+    a = RedisClient("192.168.129.212",6390,False,"xjzfw2020").redis
+    # a = RedisClient("192.168.129.213",6400,False).redis
+    print(a.info())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ODtools",
-    version="1.7.4",
+    version="1.7.6",
     author="zkrPython",
     author_email="178031608@qq.com",
     description="zkrTools",

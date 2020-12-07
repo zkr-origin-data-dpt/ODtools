14bc79cab42e6619e4cd51b55eb835a939326f29
malongfei <178031608@qq.com>
2020-12-07 15:02:07 +0800

    日志更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ODtools",
-    version="1.8.0",
+    version="1.8.2",
     author="zkrPython",
     author_email="178031608@qq.com",
     description="zkrTools",
